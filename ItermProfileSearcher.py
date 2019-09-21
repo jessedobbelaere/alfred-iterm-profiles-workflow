8f3b4bd045fc3541aa6bcc7b31087bc1057cb6ed
Terry Yao <69723+yaoxinghuo@users.noreply.github.com>
2019-09-21 21:06:36 +0800

    Cache profiles for 30 seconds for performance (#7)
    * Cache profiles for 30 seconds for performance
    * Add Alfred-Workflow
    * Support Chinese characters in profile
    
    Fixes #4

--- ItermProfileSearcher.py
+++ ItermProfileSearcher.py
@@ -1,41 +1,67 @@
 #!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 
 from lib import biplist
 import os
 import json
 import sys
+from workflow import Workflow, ICON_WARNING, MATCH_SUBSTRING
 
-# Query argument
-q = sys.argv[1]
 
-# Read iTerm's preferences file
-iTermPreferencesPath = os.path.join(os.environ["HOME"], "Library", "Preferences", "com.googlecode.iterm2.plist")
-plist = biplist.readPlist(iTermPreferencesPath)
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-# Extract profile data from plist
-profiles = {}
-for nb in plist['New Bookmarks']:
-    profileName = nb.get('Name')
-    profiles[nb.get('Name')] = {
-        'name': nb.get('Name'),
-        'command': nb.get('Command'),
-        'icon': nb.get('Custom Icon Path'),
-        'tags': nb.get('Tags')
-    }
 
-# Check if query matches any of the profile names
-if len(q) > 1:
-    profiles = {name: data for name, data in profiles.iteritems() if q.lower() in name.lower()}
+def read_profiles():
+    # Read iTerm's preferences file
+    iTermPreferencesPath = os.path.join(os.environ["HOME"], "Library", "Preferences", "com.googlecode.iterm2.plist")
+    plist = biplist.readPlist(iTermPreferencesPath)
+    # Extract profile data from plist
+    profiles = []
+    for nb in plist['New Bookmarks']:
+        profiles.append({
+            'name': nb.get('Name'),
+            'command': nb.get('Command'),
+            'icon': nb.get('Custom Icon Path'),
+            'tags': nb.get('Tags')
+        })
 
-# Generate json output for Alfred
-data = {'items': []}
-for profileName, profileData in sorted(profiles.iteritems()):
-    data['items'].append({
-        'uuid': profileName,
-        'title': profileName,
-        'subtitle': (str(profileData['tags']) if profileData['tags'] else '') + profileData['command'],
-        'icon': {'path': profileData['icon']} if profileData['icon'] else 'icon.png',
-        'arg': profileName,
-    })
+    return profiles
 
-print json.dumps(data).encode('utf-8-sig')
+
+def filter_key_for_profile(profile):
+    return profile['name'] + ' ' + str(profile['tags'])
+
+
+def sort_key_for_profile(profile):
+    return profile['name']
+
+
+def main(wf):
+    profiles = wf.cached_data('profiles', read_profiles, max_age=30)
+
+    # Query argument Ensure `query` is initialised
+    query = None
+    if len(wf.args):
+        query = wf.args[0]
+
+    profiles = wf.filter(query, profiles, filter_key_for_profile, match_on=MATCH_SUBSTRING)
+
+    if not profiles:
+        wf.add_item('No profile matches', icon=ICON_WARNING)
+
+    profiles.sort(key=sort_key_for_profile)
+    for profile in profiles:
+        wf.add_item(title=profile['name'],
+                    subtitle=((str(profile['tags']) + ' ') if profile['tags'] else '') + profile['command'],
+                    arg=profile['name'],
+                    valid=True,
+                    icon=profile['icon'])
+
+    wf.send_feedback()
+    return
+
+
+if __name__ == u"__main__":
+    wf = Workflow()
+    sys.exit(wf.run(main))
